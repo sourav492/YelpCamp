154482499d8eb38d885bb1dc83666c60d4e260ee
sourav492 <76873297+sourav492@users.noreply.github.com>
2022-08-24 21:15:06 +0530

    Added ejs-mate for a layout containing bootstrap and partials

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const methodOverride = require('method-override');
 const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate');
 const Campground = require('./models/campground')
 mongoose.connect('mongodb://localhost:27017/yelp-camp')
 const path = require('path');
@@ -13,6 +14,7 @@ db.on("error", console.error.bind(console, "connection error"))
 db.once("open", () => {
     console.log("Database connected")
 })
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
--- seeds/index.js
+++ seeds/index.js
@@ -17,9 +17,13 @@ const seedDB = async () => {
     await Campground.deleteMany({})
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000)
+        const price = Math.floor(Math.random() * 90) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://random.imagecdn.app/500/150',
+            description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non rerum dignissimos voluptate deserunt nihil nesciunt. Ex, animi temporibus nostrum molestias, tempore nihil earum quaerat, vero numquam sint dolor? Laudantium, commodi?",
+            price
         })
         await camp.save()
     }
